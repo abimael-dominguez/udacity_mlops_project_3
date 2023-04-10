670f960a354051a85a57ece642e4cc3fc7df4ced
Abimael <abimael.dominguez.perez@gmail.com>
2023-04-10 00:12:26 -0600

    load data and adapt process_data

---
+++ starter/train_model.py
@@ -0,0 +1,35 @@
+# Script to train machine learning model.
+
+from sklearn.model_selection import train_test_split
+
+# Add the necessary imports for the starter code.
+import pandas as pd
+from ml.data import process_data
+# Add code to load in the data.
+data = pd.read_csv('data/clean_census.csv', encoding='utf-8')
+
+# Optional enhancement, use K-fold cross validation instead of a train-test split.
+train, test = train_test_split(data, test_size=0.20)
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+X_train, y_train, encoder, lb = process_data(
+    train,
+    categorical_features=cat_features,
+    label="salary",
+    training=True
+)
+
+# Process the test data with the process_data function.
+
+print(dir(encoder))
+
+# Train and save a model.
